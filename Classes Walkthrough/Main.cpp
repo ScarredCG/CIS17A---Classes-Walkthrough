b4cc600570eb0bc548c95e2074a02695942c854f
ncc <ncc>
2017-02-22 09:33:32 -0800

    Student class done

--- Classes Walkthrough/Main.cpp
+++ Classes Walkthrough/Main.cpp
@@ -1,14 +1,15 @@
+#include "Student.h"
+#include <iostream>
+#include <string>
 
-
-
-
-
-
-
-
+using namespace std;
 
 int main()
 {
+	Student bob = Student("bob");
+	cout << bob.GetName() << endl;
+
+	getchar();
 
 	return 0;
 }
